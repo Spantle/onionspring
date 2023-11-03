997f551bf16481cf0b4927c8986d340e3f184966
Spantle <14240384@student.uts.edu.au>
2023-11-03 11:49:21 +1100

    small fixes

--- src/main/java/com/onionspring/app/controller/MenuController.java
+++ src/main/java/com/onionspring/app/controller/MenuController.java
@@ -50,6 +50,17 @@ public class MenuController {
         }
 
         @PostConstruct
+        public void addDummyValues() {
+                String itemName = "poptards";
+                Optional<Item> existingItem = itemRepository.findByName(itemName);
+                if (!existingItem.isPresent()) {
+                        Item item = new Item("poptards", "description", 1.99f,
+                                        "https://img.wattpad.com/90b176f068e2062cadc85d2d0f372c087e1d78e3/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f446e4c7567646c395f367a7165413d3d2d323834312e313632623930636361313461313066653339343435353135363234382e6a7067?s=fit&w=720&h=720");
+                        itemRepository.save(item);
+                }
+        }
+
+        @PostConstruct
         public void addShreghetti() {
                 String itemName = "Shreghetti";
                 Optional<Item> existingItem = itemRepository.findByName(itemName);
