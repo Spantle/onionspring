d11a58916c8e57d0c4612ad164afc39c6e94ae56
Vitrecan <vitrecan@gmail.com>
2023-11-03 11:44:26 +1100

    iuytrfcghvbijo

--- src/main/java/com/onionspring/app/controller/MenuController.java
+++ src/main/java/com/onionspring/app/controller/MenuController.java
@@ -44,12 +44,42 @@ public class MenuController {
         }
 
         @PostConstruct
-        public void addDummyValues() {
-                String itemName = "poptards";
+        public void addShreghetti() {
+                String itemName = "Shreghetti";
                 Optional<Item> existingItem = itemRepository.findByName(itemName);
                 if (!existingItem.isPresent()) {
-                        Item item = new Item("poptards", "description", 1.99f,
-                                        "https://img.wattpad.com/90b176f068e2062cadc85d2d0f372c087e1d78e3/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f446e4c7567646c395f367a7165413d3d2d323834312e313632623930636361313461313066653339343435353135363234382e6a7067?s=fit&w=720&h=720");
+                        Item item = new Item("Shreghetti",
+                                        "A delicious pasta dish with a twist of fun. Perfect for those who love Italian cuisine with a playful touch.",
+                                        6.90f,
+                                        "https://bit.ly/shreghetti");
+                        itemRepository.save(item);
+                }
+        }
+
+        @PostConstruct
+        public void addCrustyCrabbyBaguette() {
+                String itemName = "Crusty Crabby Baguette";
+                Optional<Item> existingItem = itemRepository.findByName(itemName);
+                if (!existingItem.isPresent()) {
+                        Item item = new Item("Crusty Crabby Baguette",
+                                        "A delicious pasta dish with a twist of fun. Perfect for those who love Italian cuisine with a playful touch.",
+                                        4.20f,
+                                        "https://preview.redd.it/v0c8ym4ieuv21.jpg?width=1080&crop=smart&auto=webp&s=0b4cc9d892972fa08a3d2c7d604cb2a59e5da35c");
+                        itemRepository.save(item);
+                }
+        }
+
+        @PostConstruct
+        public void addPeppasSnaughtySnacks() {
+                String itemName = "Peppa's Snaughty Snacks";
+                Optional<Item> existingItem = itemRepository.findByName(itemName);
+                if (!existingItem.isPresent()) {
+                        Item item = new Item("Peppa's Snaughty Snacks",
+                                        "An assortment of snacks perfect for a quick bite or to satisfy those mid-day cravings. Enjoy the burst of\r\n"
+                                                        + //
+                                                        "            flavors!",
+                                        80.08f,
+                                        "https://preview.redd.it/f8tx6wxdq9l51.jpg?width=1080&crop=smart&auto=webp&s=490a950421454be4cf4ced1819730974c5b1b47d");
                         itemRepository.save(item);
                 }
         }
